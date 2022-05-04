26a23ead0e9e1cf1b7d82f87c3ab4ae8d00f2ee3
paveldeuce <deucela.pavel@gmail.com>
2022-05-04 12:40:42 +0300

    fixed required and positive checks for number schema

--- src/Validator/Validator.js
+++ src/Validator/Validator.js
@@ -23,7 +23,7 @@ export default class Validator {
           return _.isNumber(value);
         },
         positive(value) {
-          return value >= 0;
+          return value > 0;
         },
         range(value, min, max) {
           return value >= min && value <= max;
@@ -31,7 +31,7 @@ export default class Validator {
       },
       array: {
         required(value) {
-          return Array.isArray(value);
+          return _.isArray(value);
         },
         sizeof(value, size) {
           return value.length === size;
